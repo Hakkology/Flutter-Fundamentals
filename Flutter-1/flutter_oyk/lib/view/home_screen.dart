c46d00053d0758b120ee86f56e492a7a6f0075f7
Hakkology <yildizhakan88@gmail.com>
2023-08-29 11:14:33 +0300

    Error corrected.

--- Flutter-1/flutter_oyk/lib/view/home_screen.dart
+++ Flutter-1/flutter_oyk/lib/view/home_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_oyk/view/detail_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  //List<Widget> userCardList = List.generate(6, (index) => _buildUserCard());
+import 'detail_screen.dart';
+
+// ignore: must_be_immutable
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
-  bool? isTrue;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String title = "Name";
+
+  bool isRead = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,88 +21,61 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Users"),
         centerTitle: true,
-        backgroundColor: Colors.amberAccent,
-        foregroundColor: Colors.blueGrey,
       ),
       body: ListView.builder(
         itemCount: 60,
         itemBuilder: (context, index) => _buildUserCard(context, index),
       ),
-      backgroundColor: const Color.fromARGB(255, 179, 171, 149),
+      backgroundColor: Colors.deepPurple,
     );
   }
 
-  Widget _buildUserCard(BuildContext context, int number) {
+  Widget _buildUserCard(BuildContext context, int index) {
     return Card(
       child: ListTile(
-        leading: const Icon(
-          Icons.person_2_rounded,
-          color: Colors.deepPurple,
+        leading: Icon(
+          Icons.person,
+          color: Colors.deepPurple.shade300,
           size: 40,
         ),
         title: Text(
-          "Name ${(number + 1)}",
+          "$title ${(index + 1)}",
           style: const TextStyle(
             fontSize: 22,
           ),
         ),
-        subtitle: const Text("Surname"),
+        subtitle: const Text(
+          "Surname",
+        ),
         trailing: IconButton(
-            icon: const Icon(
-              Icons.arrow_circle_right,
-              size: 40,
-            ),
-            onPressed: () {
-              (isTrue) => navigateToDetail(context, number);
-            }),
-        tileColor: isTrue!
-            ? Colors.amberAccent
-            : const Color.fromRGBO(240, 228, 179, 1),
+          icon: const Icon(
+            Icons.arrow_right,
+            size: 40,
+          ),
+          onPressed: () {
+            navigateToDetail(context, index);
+
+            setState(() {
+              isRead = true;
+            });
+          },
+        ),
+        tileColor: isRead ? Colors.white : Colors.deepPurple.shade100,
       ),
     );
   }
 
-  Future<bool> navigateToDetail(BuildContext context, int index) async {
-    bool isTrue =
-        await Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-      return DetailScreen(
-        appBarTitle: "Name ${(index + 1)}",
-      );
-    }));
+  Future<void> navigateToDetail(BuildContext context, int index) async {
+    bool isTrue = await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return DetailScreen(
+            appBarTitle: "$title ${(index + 1)}",
+          );
+        },
+      ),
+    );
     // ignore: avoid_print
-    print("Is it true: $isTrue");
-    return isTrue;
+    print("TRUE MU:  $isTrue");
   }
 }
-
-// class _HomeScreenState extends State<HomeScreen> {
-//   @override
-//   void initState() {
-//     print("initState");
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     print("buildState");
-//     return Scaffold(
-//         body: Column(
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         Center(
-//           child: Container(
-//             width: 100,
-//             height: 100,
-//             color: Colors.amberAccent,
-//           ),
-//         ),
-//       ],
-//     ));
-//   }
-
-//   @override
-//   void dispose() {
-//     print("dispose");
-//     super.dispose();
-//   }
-// }
